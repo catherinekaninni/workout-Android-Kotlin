59e696ccc643d1adf6009bd974d97a669b41531c
cathernekaninni <catherinekaninni@gmail.com>
2022-05-24 18:29:11 +0300

    first commit

---
+++ app/src/main/java/dev/kanini/workoutlog/LoginActivity.kt
@@ -0,0 +1,11 @@
+package dev.kanini.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/dev/kanini/workoutlog/SplashActivity.kt
@@ -0,0 +1,15 @@
+package dev.kanini.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
