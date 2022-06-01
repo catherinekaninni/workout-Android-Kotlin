6ca16b0d5018de92b74c5c497f5c94c32e009f5c
cathernekaninni <catherinekaninni@gmail.com>
2022-06-01 22:59:04 +0300

    Signup

--- app/src/main/java/dev/kanini/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/kanini/workoutlog/LoginActivity.kt
@@ -1,11 +1,49 @@
 package dev.kanini.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var tvsignup:TextView
+    lateinit var etEmail:TextInputEditText
+    lateinit var etPassword:TextInputEditText
+    lateinit var tilEmail:TextInputLayout
+    lateinit var tilPassword:TextInputLayout
+    lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvsignup=findViewById(R.id.tvsignup)
+        etEmail=findViewById(R.id.etEmail)
+        etPassword=findViewById(R.id.etPassword)
+        btnLogin=findViewById(R.id.btnLogin)
+        tilEmail=findViewById(R.id.tilEmail)
+        tilPassword=findViewById(R.id.tilPassword)
+        tvsignup.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        btnLogin.setOnClickListener {
+            validate()
+
+
+        }
+
+    }
+    fun validate(){
+        val Email=etEmail.text.toString()
+        val Password=etPassword.text.toString()
+        if (Email.isBlank()){
+            tilEmail.error="invalid"
+        }
+        if (Password.isBlank()){
+            tilPassword.error="invalid"
+        }
     }
 }
---
+++ app/src/main/java/dev/kanini/workoutlog/SignUpActivity.kt
@@ -0,0 +1,71 @@
+package dev.kanini.workoutlog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class SignUpActivity : AppCompatActivity() {
+    lateinit var etfirstname:EditText
+    lateinit var tilfirstname:TextInputLayout
+    lateinit var etlastname:EditText
+    lateinit var tilastname:TextInputLayout
+    lateinit var etemail:EditText
+    lateinit var tilemail:TextInputLayout
+    lateinit var etpassword:EditText
+    lateinit var tilpassword:TextInputLayout
+    lateinit var etconfirm:EditText
+    lateinit var tilconfirm:TextInputLayout
+    lateinit var btnsignup:Button
+    lateinit var tvlogin:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        etfirstname=findViewById(R.id.etfirstname)
+        tilfirstname=findViewById(R.id.tilfirstname)
+        etlastname=findViewById(R.id.etlastname)
+        tilastname=findViewById(R.id.tilastname)
+        etemail=findViewById(R.id.etemail)
+        tilemail=findViewById(R.id.tilemail)
+        etpassword=findViewById(R.id.etpassword)
+        tilpassword=findViewById(R.id.tilpassword)
+        etconfirm=findViewById(R.id.etconfirm)
+        tilconfirm=findViewById(R.id.tilconfirm)
+        btnsignup=findViewById(R.id.btnsignup)
+        tvlogin=findViewById(R.id.tvlogin)
+        tvlogin.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        btnsignup.setOnClickListener {
+            validate()
+        }
+
+    }
+    fun validate(){
+        val firstname=etfirstname.text.toString()
+        val lastname=etlastname.text.toString()
+        val email=etemail.text.toString()
+        val password=etpassword.text.toString()
+        val confirm=etconfirm.text.toString()
+        if (firstname.isBlank()){
+            tilfirstname.error="invalid"
+        }
+        if (lastname.isBlank()){
+            tilastname.error="invalid"
+        }
+        if (email.isBlank()){
+            tilemail.error="invalid"
+        }
+        if (password.isBlank()){
+            tilpassword.error="invalid"
+        }
+        if (confirm.isBlank()){
+            tilconfirm.error="invalid"
+        }
+
+    }
+}
